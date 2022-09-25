8fbb0f205e5b1a31ab07d1f16eb18a15decb4e67
Diaa Awad <diaa.awad@itworxedu.com>
2022-09-25 12:21:34 +0200

    build remove & display methods

--- heap/Heap.py
+++ heap/Heap.py
@@ -1,14 +1,22 @@
+import sys
+
+
 class Heap(object):
-    def __init__(self):
-        self.items = []
-        self.size = 0
+    def __init__(self, max_size):
+        self._max_size = max_size
+        self._items = [0] * (self._max_size + 1)
+        self._items[0] = -1 * sys.maxsize
+        self._size = 0
+        self._front = 1
 
     def insert(self, item):
-        self.size += 1
-        self.items[self.size] = item
+        if self.is_full():
+            raise Exception("Heap Full")
+        self._size += 1
+        self._items[self._size] = item
 
-        current = self.size
-        while self.items[current] < self.items[self._parent(current)]:
+        current = self._size
+        while self._items[current] < self._items[self._parent(current)]:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
@@ -25,14 +33,14 @@ class Heap(object):
         return (pos * 2) + 1
 
     def _is_leaf(self, pos):
-        return (pos * 2) > self.size
+        return (pos * 2) > self._size
 
     def _swap(self, first, second):
-        self.items[first], self.items[second] = self.items[second], self.items[first]
+        self._items[first], self._items[second] = self._items[second], self._items[first]
 
     def min_heapify(self, pos):
         if not self._is_leaf(pos):
-            if self._left_child(pos) < self.items[pos] or self._right_child(pos) < self.items[pos]:
+            if self._left_child(pos) < self._items[pos] or self._right_child(pos) < self._items[pos]:
                 if self._left_child(pos) < self._right_child(pos):
                     self._swap(pos, self._left_child(pos))
                     self.min_heapify(self._left_child(pos))
@@ -41,5 +49,40 @@ class Heap(object):
                         self._swap(pos, self._right_child(pos))
                         self.min_heapify(self._right_child(pos))
 
-    def remove(self, item):
-        pass
+    def remove(self):
+        if self.is_empty():
+            raise Exception("Empty heap")
+
+        removed_item = self._items[self._front]
+        self._items[self._front] = self._items[self._size]
+        self._items[self._size] = 0
+        self._size -= 1
+        self.min_heapify(self._front)
+        return removed_item
+
+    def size(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def is_full(self):
+        return self._size >= self._max_size
+
+    def display(self):
+        for i in range(1, (self._size // 2) + 1):
+            print(" PARENT : " + str(self._items[i]) + " LEFT CHILD : " +
+                  str(self._items[2 * i]) + " RIGHT CHILD : " +
+                  str(self._items[2 * i + 1]))
+
+
+heap = Heap(20)
+heap.insert(5)
+heap.insert(10)
+heap.insert(1)
+heap.insert(3)
+heap.insert(11)
+heap.insert(2)
+# heap.remove()
+
+heap.display()
