80006a9be52aaa0c864ac58c66c7f7d35173bc48
Diaa Ramadan <36800905+DiaaRamadan@users.noreply.github.com>
2022-10-02 23:07:48 +0200

    add quick sort

---
+++ sort algorithms/quick_sort.py
@@ -0,0 +1,34 @@
+def quick_sort(items: list):
+    sort_all(items, 0, len(items) - 1)
+
+
+def sort_little_bit(items, begin_index, end_index):
+    left_index = begin_index
+    pivot_index = end_index
+    pivot_value = items[pivot_index]
+
+    while pivot_index != left_index:
+        item = items[left_index]
+        if item <= pivot_value:
+            left_index += 1
+            continue
+        items[left_index] = items[pivot_index - 1]
+        items[pivot_index - 1] = pivot_value
+        items[pivot_index] = item
+        pivot_index -= 1
+
+    return pivot_index
+
+
+def sort_all(items, begin_index, end_index):
+    if begin_index >= end_index:
+        return
+
+    pivot_index = sort_little_bit(items, begin_index, end_index)
+    sort_all(items, begin_index, pivot_index - 1)
+    sort_all(items, pivot_index + 1, end_index)
+
+
+list = [8, 3, 1, 7, 0, 10, 2]
+quick_sort(list)
+print(list)
